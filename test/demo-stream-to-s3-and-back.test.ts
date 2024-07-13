0d5ab5b2b4bb0155c9c6bd848b60fdf8e2c433e2
Dennis O'Keeffe <hello@dennisokeeffe.com>
2024-07-13 10:07:10 +1000

    Initial commit

---
+++ test/demo-stream-to-s3-and-back.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DemoStreamToS3AndBack from '../lib/demo-stream-to-s3-and-back-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/demo-stream-to-s3-and-back-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DemoStreamToS3AndBack.DemoStreamToS3AndBackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
